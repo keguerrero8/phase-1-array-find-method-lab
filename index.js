681e78ab3f6b059255aadf9f0721858b2526c445
Kevin Guerrero <guerrk83z@gmail.com>
2021-11-23 22:06:02 -0500

    commit tree 332454d401eed8a28495c305bb41696ef8214c7b

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+function superbowlWin(obj) {
+        let objElement = obj.find(element => element.result === "W") //this will return element
+        if (objElement === undefined) {
+            return objElement
+        } 
+        return objElement.year
+}
+
 
